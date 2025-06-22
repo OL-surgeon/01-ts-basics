84462352e0327e5fe54e71fd46b3873f8673795f
OL-surgeon <dasti888@ukr.net>
2025-06-22 17:08:44 +0300

    .

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,10 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]); // 1
-getFirstElement(["a", "b", "c"]); // "a"
-getFirstElement([true, false, true]); // true
+const firstNumber = getFirstElement<number>([1, 2, 3]);
+const firstString = getFirstElement<string>(["a", "b", "c"]);
+const firstBoolean = getFirstElement<boolean>([true, false]);
+
+console.log(firstNumber);
+console.log(firstString);
+console.log(firstBoolean);
